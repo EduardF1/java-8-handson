65bfdfe3bc3814d400ba36fad95634d26d6a1178
EduardF1 <fischer_eduard@yahoo.com>
2021-10-17 19:13:27 +0200

    .

--- language/src/main/java/operations/StreamMap.java
+++ language/src/main/java/operations/StreamMap.java
@@ -2,13 +2,14 @@ package operations;
 
 import function.dto.Person;
 import function.repository.PersonRepository;
-import method_reference.SimpleMethodReference;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StreamMap {
@@ -20,6 +21,13 @@ public class StreamMap {
                 .map(Person::getName)
                 .map(String::toUpperCase)
                 .collect(Collectors.toList());
+    }
+
+    static Set<String> toUpperTransformSet(List<Person> persons) {
+        return persons.stream()
+                .map(Person::getName)
+                .map(String::toUpperCase)
+                .collect(Collectors.toSet());
     }
 
     public static void main(String[] args) {
@@ -37,5 +45,6 @@ public class StreamMap {
                 .forEach(logger::info);
 
         logger.info(toUpperTransform(PersonRepository.getAllPersons()));
+        logger.info(toUpperTransformSet(PersonRepository.getAllPersons()));
     }
 }
