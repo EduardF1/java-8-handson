83d393be39114e6de3501b3eaab2510d151f8835
EduardF1 <fischer_eduard@yahoo.com>
2021-10-17 23:08:40 +0200

    .

--- language/src/main/java/numeric_stream/NumericStreamMap.java
+++ language/src/main/java/numeric_stream/NumericStreamMap.java
@@ -4,6 +4,8 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class NumericStreamMap {
@@ -13,11 +15,25 @@ public class NumericStreamMap {
         BasicConfigurator.configure();
 
         logger.info("Map to double: " + mapIntegersToDouble());
+        logger.info("Map to long: " + mapIntegersToLong());
+        logger.info("Map to object: " + mapToObject());
     }
 
-    private static double mapIntegersToDouble(){
-        return IntStream.rangeClosed(1,10)          //  primitive int elements
+    private static double mapIntegersToDouble() {
+        return IntStream.rangeClosed(1, 10)          //  primitive int elements
                 .mapToDouble(x -> x)                //  converting the elements to double
                 .sum();                             //  performing sum
+    }
+
+    private static long mapIntegersToLong() {
+        return IntStream.rangeClosed(1, 10)
+                .mapToLong(x -> x)
+                .sum();
+    }
+
+    private static List<Integer> mapToObject() {
+        return IntStream.rangeClosed(1, 10)
+                .mapToObj(x -> x)
+                .collect(Collectors.toList());
     }
 }
