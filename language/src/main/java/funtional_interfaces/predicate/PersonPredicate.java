802c384246d330ca54193da5f787fedb0ca4bead
EduardF1 <fischer_eduard@yahoo.com>
2021-10-17 16:00:05 +0200

    add predicates and filter

---
+++ language/src/main/java/funtional_interfaces/predicate/PersonPredicate.java
@@ -0,0 +1,28 @@
+package funtional_interfaces.predicate;
+
+import funtional_interfaces.dto.Person;
+import funtional_interfaces.repository.PersonRepository;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class PersonPredicate {
+    private static final Logger logger = LogManager.getLogger(PersonPredicate.class);
+
+    static Predicate<Person> isHeightGreaterThan140 = person -> person.getHeight() >= 140;
+    static Predicate<Person> isGenderMale = person -> person.getGender().equals("Male");
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+
+        PersonRepository.getAllPersons()
+                .stream()
+                .filter(isHeightGreaterThan140.and(isGenderMale))
+                .collect(Collectors.toList())
+                .forEach(person -> logger.info(person.toString()));
+    }
+}
---
+++ language/src/main/java/funtional_interfaces/predicate/SimplePredicate.java
@@ -0,0 +1,28 @@
+package funtional_interfaces.predicate;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.util.function.Predicate;
+
+public class SimplePredicate {
+    private static final Logger logger = LogManager.getLogger(SimplePredicate.class);
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+
+        Predicate<Integer> lessThan50 = a -> (a <= 50);
+        Predicate<Integer> greaterThan20 = a -> (a >= 20);
+        Predicate<Integer> equalTo0 = a -> (a == 0);
+
+        logger.info(lessThan50.test(51));
+        logger.info(greaterThan20.test(11));
+        logger.info(equalTo0.test(11));
+
+        logger.info(greaterThan20.and(lessThan50).or(equalTo0).test(31));
+        logger.info(equalTo0.or(lessThan50).test(21));
+        logger.info(greaterThan20.and(lessThan50).test(31));
+        logger.info(greaterThan20.and(equalTo0).negate().test(21));
+    }
+}
